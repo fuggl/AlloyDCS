22c8e63d9922f6b69430f5d560cd2e29ce8ea023
Sebastian Fuchs <fuchs.se.de@gmail.com>
2016-05-12 18:59:49 +0200

    added class: WindowCloseCleanupService

---
+++ alloydcs/FX/src/main/java/org/alloydcs/fx/cleanup/WindowCloseCleanupService.java
@@ -0,0 +1,11 @@
+package org.alloydcs.fx.cleanup;
+
+import javafx.scene.Node;
+
+public final class WindowCloseCleanupService extends CleanupService {
+
+	public WindowCloseCleanupService(final Node initializedNode) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
