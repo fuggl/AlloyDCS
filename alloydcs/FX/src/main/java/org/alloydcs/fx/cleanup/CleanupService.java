f3f3a67701e308405c7f5db03ad1435525b51260
Sebastian Fuchs <fuchs.se.de@gmail.com>
2016-05-12 18:55:51 +0200

    added array list for jobs

--- alloydcs/FX/src/main/java/org/alloydcs/fx/cleanup/CleanupService.java
+++ alloydcs/FX/src/main/java/org/alloydcs/fx/cleanup/CleanupService.java
@@ -1,12 +1,28 @@
 /**
- * 
+ *
  */
 package org.alloydcs.fx.cleanup;
+
+import java.util.ArrayList;
 
 /**
  * @author sebastian
  *
  */
 public abstract class CleanupService {
+
+	private final ArrayList<CleanupJob> jobs;
+
+	public CleanupService() {
+		this.jobs = new ArrayList<CleanupJob>();
+	}
+
+	public void addJob(final CleanupJob job) {
+		this.jobs.add(job);
+	}
+
+	protected void cleanup() {
+		this.jobs.forEach(job -> job.cleanup());
+	}
 
 }
