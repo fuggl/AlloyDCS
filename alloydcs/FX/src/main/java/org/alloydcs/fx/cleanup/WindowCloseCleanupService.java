0e8db538bd305fdf7516bb5cd55e718f49365e50
Sebastian Fuchs <fuchs.se.de@gmail.com>
2016-05-26 08:47:08 +0200

    added creation of listener cascade to ensure cleanup on window close

--- alloydcs/FX/src/main/java/org/alloydcs/fx/cleanup/WindowCloseCleanupService.java
+++ alloydcs/FX/src/main/java/org/alloydcs/fx/cleanup/WindowCloseCleanupService.java
@@ -1,11 +1,37 @@
 package org.alloydcs.fx.cleanup;
 
+import javafx.beans.value.ChangeListener;
+import javafx.event.EventHandler;
 import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.stage.Window;
+import javafx.stage.WindowEvent;
 
 public final class WindowCloseCleanupService extends CleanupService {
 
 	public WindowCloseCleanupService(final Node initializedNode) {
-		// TODO Auto-generated constructor stub
+		initializedNode.sceneProperty().addListener(
+				WindowCloseCleanupService.createSceneChangeListener(
+						WindowCloseCleanupService.createWindowChangeListener(
+								WindowCloseCleanupService.createCleanupHandler(
+										this))));
+	}
+
+	private static EventHandler<WindowEvent> createCleanupHandler(
+			final CleanupService cleanupService) {
+		return event -> cleanupService.cleanup();
+	}
+	private static ChangeListener<Window> createWindowChangeListener(
+			final EventHandler<WindowEvent> eventHandler) {
+		return (observable, oldValue, newValue) -> {
+			newValue.addEventHandler(WindowEvent.WINDOW_HIDDEN, eventHandler);
+		};
+	}
+	private static ChangeListener<Scene> createSceneChangeListener(
+			final ChangeListener<Window> windowChangeListener) {
+		return (observable, oldValue, newValue) -> {
+			newValue.windowProperty().addListener(windowChangeListener);
+		};
 	}
 
 }
